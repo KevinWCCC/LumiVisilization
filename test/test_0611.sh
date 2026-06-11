e3a24a514a1aaf6401cd6f9fc6cc212b81bf2497
wckwckwckwck <wckwckwckwck@163.com>
2026-06-11 20:58:36 +0800

    [comment] 0610 添加可视化代码 离散化构造点

---
+++ test/test_0611.sh
@@ -0,0 +1,10 @@
+# python real_pixel_embedding_tsne_0610.py\
+#     --dataset koadk\
+
+# python real_pixel_embedding_tsne_0610_02.py\
+#     --dataset koadk\
+
+python real_pixel_embedding_tsne_0610_2.py\
+    --dataset koadk\
+
+exit
