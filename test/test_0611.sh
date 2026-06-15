3c3f7a36fd8754e8eaa17994ee3a72c62dfafc6f
KevinWCCC <wckwckwckwckg@gmail.com>
2026-06-15 12:40:01 +0800

    [06.15] 添加代码

--- test/test_0611.sh
+++ test/test_0611.sh
@@ -4,7 +4,10 @@
 # python real_pixel_embedding_tsne_0610_02.py\
 #     --dataset koadk\
 
-python real_pixel_embedding_tsne_0610_2.py\
+# python real_pixel_embedding_tsne_0610_2.py\
+#     --dataset koadk\
+
+python real_pixel_embedding_tsne_0610_3.py\
     --dataset koadk\
 
 exit
