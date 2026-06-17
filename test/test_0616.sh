299ebd35c89ac139f5a89e0a5b53f470c82a6f93
wckwckwckwck <wckwckwckwck@163.com>
2026-06-17 10:19:27 +0800

    [comment] 06.16 添加按照通道颜色区分图标

---
+++ test/test_0616.sh
@@ -0,0 +1,7 @@
+python read_pixel_three_panel_comparison.py \
+    --model_id /home/vipuser/Model/LLAMA_3.1_B \
+    --dataset koadk \
+    --ckpt_dir models \
+    --use_best \
+    --yellow_jitter_range 5 \
+    --output pixel_embedding_three_panel_lumi_vs_p2llm.png
